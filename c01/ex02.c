60432a08d69f83d116f4c0911e5cbbee8e1f807d
unknown <elghazouani4b@gmail.com>
2025-04-24 14:12:50 +0100

    c01

--- c01/ex02.c
+++ c01/ex02.c
@@ -1,1 +1,18 @@
+#include <unistd.h>
+// #include <stdio.h>
 
+void ft_swap(int *a, int *b){
+    int swap = *a;
+    *a = *b ;
+    *b = swap ;
+}
+
+// int main(){
+//     int n1 = 10;
+//     int *p1 = &n1;
+//     int n2 = 20;
+//     int *p2 = &n2;
+//     ft_swap(p1,p2);
+//     printf("n1 value  = %d\n",n1);
+//     printf("n2 value = %d",n2);
+// }
