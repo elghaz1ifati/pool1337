782f29b26688f1611ad406b0668520ec8a5d8c0b
unknown <elghazouani4b@gmail.com>
2025-05-05 12:55:09 +0100

    c01

---
+++ c01/ex04.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include <stdio.h>
+
+void ft_ultimate_div_mod(int *a, int *b){
+    int div , mod ;
+    div = *a / *b;
+    mod = *a % *b;
+    *a = div;
+    *b = mod;
+}
+int main()
+{
+    int a = 10;
+    int b = 2;
+    ft_ultimate_div_mod(&a,&b);
+    printf("%d\n",a);
+    printf("%d\n",b);
+    
+}
+
